0d32fdd46a6c42a365d02142aca3516f12a85bbf
kk-deng <71301799+kk-deng@users.noreply.github.com>
2021-01-25 13:15:01 -0500

    Update app.js

--- D3_Data_Journalism/assets/js/app.js
+++ D3_Data_Journalism/assets/js/app.js
@@ -1,6 +1,56 @@
-// Loading the file path
-var filepath = "../assets/data/data.csv"
+var svgWidth = 960;
+var svgHeight = 500;
 
-d3.csv(filepath).then(data => {
-    console.log(data)
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+// Initial Params
+var chosenXAxis = "poverty";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(censusData, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+        d3.max(hairData, d => d[chosenXAxis]) * 1.2
+        ])
+        .range([0, width]);
+
+    return xLinearScale;
+
+}
+
+// Loading the file
+d3.csv("assets/data/data.csv").then((censusData, err) => {
+    if (err) throw err;
+    // parse data
+    censusData.forEach(data => {
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+        data.smokes = +data.smokes;
+        data.age = +data.age;
+    });
+
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(hairData, chosenXAxis);
 })
